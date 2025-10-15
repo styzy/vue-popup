4661260ea3a9a99091f155dd2344eb71fb41cb8b
styzy <styzyreal@126.com>
2025-10-15 14:49:10 +0800

    feat: 新增插件定义方法definePlugin用于定义插件

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,33 @@ import { ANIMATION_TYPES, AnimationType } from './CONSTANTS'
 import { typeOf, deepClone } from './utils'
 import PACKAGE_JSON from '../package.json'
 
-type VuePopupInstall = (_Popup: typeof Popup, Vue: VueConstructor) => void
-
 type VuePopupPlugin = {
+	/**
+	 * 插件名称
+	 */
 	name: string
+	/**
+	 * 插件安装函数
+	 * - 提供两个参数
+	 *   - 第一个参数为 Popup 类
+	 *   - 第二个参数为 Vue 构造函数
+	 */
 	install: VuePopupInstall
 }
 
+export function definePlugin<TPlugin extends VuePopupPlugin>(
+	plugin: TPlugin
+): TPlugin {
+	return plugin
+}
+
+type VuePopupInstall = (_Popup: typeof Popup, Vue: VueConstructor) => void
+
 type VuePopupOptions = {
+	/**
+	 * 全局的 z-index 层级，默认值为 1000
+	 * - 每个弹框渲染都会自动递增这个值
+	 */
 	zIndex?: number
 }
 
@@ -21,28 +40,120 @@ type VuePopupRenderOptions = {
 	 * 是否显示遮罩层
 	 */
 	mask?: boolean
+	/**
+	 * 是否点击遮罩层关闭弹窗
+	 */
 	maskClickClose?: boolean
+	/**
+	 * 弹窗组件
+	 * - 可以使用组件或异步组件
+	 * - 使用示例：
+	 * ```
+	 * // 同步组件
+	 * import MyComponent from './MyComponent.vue'
+	 * popup.render({
+	 * 	component: MyComponent
+	 * })
+	 * ```
+	 * ```
+	 * // 异步组件
+	 * popup.render({
+	 * 	component: () => import('./MyComponent.vue')
+	 * })
+	 * ```
+	 */
 	component: Component
+	/**
+	 * 弹窗组件的 props
+	 * - 可以传递任意 props 给弹窗组件
+	 */
 	componentProps?: Record<string, any>
+	/**
+	 * 弹窗遮罩的动画类型
+	 * - 默认值为 [ANIMATION_TYPES.FADE]
+	 * - 可以设置为 [ANIMATION_TYPES.FADE, ANIMATION_TYPES.SLIDE] 等
+	 */
 	maskAnimations?: Array<AnimationType>
+	/**
+	 * 弹窗视图的动画类型
+	 * - 默认值为 [ANIMATION_TYPES.FADE]
+	 * - 可以设置为 [ANIMATION_TYPES.FADE, ANIMATION_TYPES.SLIDE] 等
+	 */
 	viewAnimations?: Array<AnimationType>
+	/**
+	 * 弹窗动画的持续时间，单位为毫秒
+	 * - 默认值为 300
+	 */
 	animationDuration?: number
+	/**
+	 * 弹窗的宽度
+	 * - 默认值为 'auto'
+	 */
 	width?: string | number
+	/**
+	 * 弹窗的最大宽度
+	 * - 默认值为 'auto'
+	 */
 	maxWidth?: string | number
+	/**
+	 * 弹窗的最小宽度
+	 * - 默认值为 'auto'
+	 */
 	minWidth?: string | number
+	/**
+	 * 弹窗的高度
+	 * - 默认值为 'auto'
+	 */
 	height?: string | number
+	/**
+	 * 弹窗的最大高度
+	 * - 默认值为 'auto'
+	 */
 	maxHeight?: string | number
+	/**
+	 * 弹窗的最小高度
+	 * - 默认值为 'auto'
+	 */
 	minHeight?: string | number
+	/**
+	 * 弹窗挂载完成后的回调函数
+	 */
 	mounted?: (instance: Vue) => void
+	/**
+	 * 弹窗销毁后的回调函数
+	 */
 	destroyed?: (payload?: any) => void
+	/**
+	 * 弹窗的 z-index 层级
+	 * - 默认自动从全局的 z-index 层级递增
+	 */
 	zIndex?: number
 }
 
 declare class VuePopup {
+	/**
+	 * 版本号
+	 */
 	static get version(): string
-	static get plugins(): Array<VuePopupPlugin>
+	/**
+	 * 安装函数，提供给 Vue.use() 安装插件
+	 */
 	static install: PluginFunction<void>
 	constructor(options: VuePopupOptions)
+	/**
+	 * 渲染弹窗
+	 * @param options 弹窗渲染选项
+	 * @returns 关闭弹窗的函数
+	 * - 调用该函数可以关闭弹窗
+	 * - 使用示例：
+	 * ```
+	 * const close = popup.render({
+	 * 	component: MyComponent
+	 * })
+	 * // 关闭弹窗
+	 * close()
+	 * ```
+	 */
 	render(options: VuePopupRenderOptions): () => void
 }
 
@@ -51,16 +162,11 @@ type PopupInstance = {
 	instance?: InstanceType<VueConstructor>
 }
 
-const plugins: Record<string, VuePopupInstall> = {}
-
 let _Vue: VueConstructor, rootVm: Vue
 
-class Popup implements VuePopup {
+export default class Popup implements VuePopup {
 	static get version() {
 		return PACKAGE_JSON.version
-	}
-	static get plugins() {
-		return plugins
 	}
 	static get ANIMATION_TYPES() {
 		return deepClone(ANIMATION_TYPES)
@@ -96,6 +202,7 @@ class Popup implements VuePopup {
 
 		this._usePlugin(plugin)
 	}
+	static plugins: Record<string, VuePopupInstall> = {}
 	private _seed = 0
 	private _zIndex: number
 	private _popups: Record<string, PopupInstance> = {}
@@ -204,5 +311,3 @@ class Popup implements VuePopup {
 		}
 	}
 }
-
-export default Popup
